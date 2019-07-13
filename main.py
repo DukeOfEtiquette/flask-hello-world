adf1b9d2c423991c50ee5af13e569830c9fe288d
DukeOfEtiquette <duquetteadam@gmail.com>
2019-07-13 08:12:33 -0700

    clean up code

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from settings import PORT
+from settings import NAME
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    print('PORT:', PORT) 
-    name="Adam"
+    print('Adam:', Adam)
+    f"NAME: {NAME}"
     return render_template("hello.html", name=name)
+
 
 @app.route('/sales')
 def sales():
-    # return "Hello sales"
     return render_template("sales.html", total=42)
